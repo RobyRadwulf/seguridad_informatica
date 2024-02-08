24b59d86b4583c5168ede6a8fe98ea8f94b2ab1c
robyr <robyradwulf@gmail.com>
2024-02-07 20:02:06 -0600

    Eve, Alice & Bob

--- DiffieHellman.py
+++ DiffieHellman.py
@@ -1,5 +1,6 @@
 import random
 import hashlib
+from tkinter import E
 
 # 1. Deberá hacer g=2 e investigar el número primo p
 g = 2
@@ -10,21 +11,40 @@ a = random.getrandbits(256)
 print("\n""La llave privada de Alice es:", a)
 b = random.getrandbits(256)
 print("\n""La llave privada de Bob es:", b)
+e = random.getrandbits(256)
+print("\n""La llave privada de Eve es:", e)
 
 # 3. Simular el intercambio de números entre Alice y Bob
 A = pow(g, a, p)
 print("\n""El número que Alice le envía a Bob es:", A)
 B = pow(g, b, p)
 print("\n""El número que Bob le envía a Alice es:", B)
+E = pow(g, e, p)
+print("\n""El número que Eve le envía a Alice es:", E)
+
 
 # 4. Hacer el cómputo de la llave secreta “s” y el modulo de la llave secreta
-s1 = pow(B, a, p)
+s1 = pow(E, a, p)
 print("\n""La llave secreta de Alice es:", s1)
-s2 = pow(A, b, p)
-print("\n""La llave secreta de Bob es:", s2)
+s2 = pow(A, e, p)
+print("\n""La llave secreta de eve/alice es:", s2)
+s3 = pow(E, b, p)
+print("\n""La llave secreta de Bob es:", s3)
+s4 = pow(B, e, p)
+print("\n""La llave secreta de eve/bob es:", s4)
 
 if s1 == s2:
     s = hashlib.sha256(str(s1).encode()).hexdigest()
     print("\n""La llave secreta es:", s)
 else:
     print("Error: Las llaves secretas no coinciden.")
+if s3 == s4:
+    s = hashlib.sha256(str(s3).encode()).hexdigest()
+    print("\n""La llave secreta es:", s)
+else:
+    print("Error: Las llaves secretas no coinciden.")
+
+if s1 == s3:
+    print("\n""Las llaves secretas de Alice y Bob coinciden.")
+else:
+    print("\n""Error: Las llaves secretas no coinciden.""\n")
